c13d1a340805c997aff2b2688abab7df4c70fa02
Rishant singh <rishantsingh0707@gmail.com>
2026-01-30 20:21:32 +0530

    Added CORS Function

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
+
 app.use(express.json());
 app.use(requestLogger);
 
